7ac258e90edfb526aacc1951fe13ef5f0d0ff819
tiaonazario <tiaogogo@gmail.com>
2023-01-18 11:03:35 -0300

    feat: set the cors for fastify

--- nlw/copa/server/src/server.ts
+++ nlw/copa/server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
 
+import cors from "@fastify/cors";
+
 const prisma = new PrismaClient({
   log: ["query"],
 });
@@ -10,13 +12,17 @@ async function bootstrap() {
     logger: true,
   });
 
+  await fastify.register(cors, {
+    origin: true,
+  });
+
   fastify.get("/pools/count", async () => {
     const pools = await prisma.pool.count();
 
     return { pools };
   });
 
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333, host: "0.0.0.0" });
 }
 
 bootstrap();
